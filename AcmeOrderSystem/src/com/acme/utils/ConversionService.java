d49e2b1394d5873dd6e2383713c5490c3712463f
JorgeAcostaDLP <jh.acostadelapena@gmail.com>
2020-06-25 13:55:00 -0400

    Spacing

--- AcmeOrderSystem/src/com/acme/utils/ConversionService.java
+++ AcmeOrderSystem/src/com/acme/utils/ConversionService.java
@@ -6,22 +6,29 @@ public class ConversionService {
 	/**
 	 * weight conversion rates
 	 */
+	
 	// conversion rate for 1 kilogram to pounds
-	public static BigDecimal kilogramToPounds = new
-			BigDecimal(2.2046);
+	public static BigDecimal kilogramToPounds = new BigDecimal(2.2046);
+	
 	// conversion rate for 1 kilogram to grams
 	public static BigDecimal kilogramToGrams = new BigDecimal(1000);
+
 	// conversion rate for 1 kilogram to weight ounces
 	public static BigDecimal kilogramToOunces = new BigDecimal(35.274);
+	
 	/**
 	 * volume conversion rates
 	 */
+	
 	// conversion rate for 1 liter to fluid ounces
 	public static float literToFluidOunce = 33.814f;
+	
 	// conversion rate for 1 liter to gallons
 	public static float literToGallon = 0.2642f;
+	
 	// conversion rate for 1 liter to pints
 	public static float literToPints = 2.1134f;
+	
 	// conversion rate for 1 liter to milliliters
 	public static short litertoMilliliters = 1_000;
 
